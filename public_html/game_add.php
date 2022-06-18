db3cbf39c663797480518eee3a8753a4d84308e3
Mimori256 <mimorimori256@gmail.com>
2022-06-18 21:36:15 +0900

    update

--- public_html/game_add.php
+++ public_html/game_add.php
@@ -9,6 +9,22 @@ function add_quoations($text) {
 }
 
 
+function update_overall_result($overall_result, $result) {
+  $tmp_list = explode("-", $overall_result);
+  $win = intval(tmp_list[0]);
+  $lose = intval(tmp_list[1]);
+  if ($result) {
+    $win++;
+  } else {
+    $lose++;
+  }
+  $new_overall_result = strval($win) . "-" . strval($lose);
+
+  return $new_overall_result;
+}
+  
+
+
 function form_element($element) {
   $element = str_replace('[', '', $element);
   $element = str_replace(']', '', $element);
@@ -44,9 +60,17 @@ function parse_time($time_range) {
 
 function check_winner($result, $color) {
   if ($result == '1-0') {
-    return $color == 'white';
+    if ($color == 'white') {
+      return 'true';
+    } else {
+      return 'false';
+    }
   } else {
-    return $color == 'black';
+    if ($color == 'black') {
+      return 'true';
+    } else {
+      return 'false';
+    }
   }
 }
 
@@ -104,16 +128,47 @@ $time_range = add_quoations($tmp_list[2]);
 $opening = add_quoations($tmp_list[3]);
 $moves = add_quoations($tmp_list[4]);
 $opponent = add_quoations($tmp_list[5]);
-$values = join("," , [$date, $color, $result, $time_range, $opening, $moves, $opponent]);
-
-$sql = "insert into result (date,  color, gameResult, timeRange, opening, moves, opponent) values (" .$values. ")";
+$values = join("," , [$date, $color,  $result, $time_range, $opening, $moves, $opponent]);
+$sql = "insert into result (date,  color, gameResult, timeRange, opening, moves, opponentName) values (" .$values. ")";
 print($sql);
-print("\n");
 
 $res = mysqli_query($conn, $sql);
-$mysqli_free_result($res);
+#$mysqli_free_result($res);
 
-print($res);
+#対戦相手が新しい相手かチェックする
+$sql = 'select * from opponent';
+print($sql);
+$res = mysqli_query($conn, $sql);
+$name_list = [];
+$is_contain_name = false;
+$overall_result = "";
+
+while ($row = mysqli_fetch_array($res)) {
+  if ($row["name"] == $opponent) {
+    $overall_result = $row["overallResult"];
+  }
+}
+
+print_r($name_list);
+
+if (!$is_contain_name)) {
+  #新しくリストに追加
+  $overall_result = '';
+  if ($result == 'true') {
+    $overall_result = '"1-0"';
+  } else {
+    $overall_result = '"0-1"';
+  }
+  $values = join(",", [$opponent, $overall_result]);
+  $sql = 'insert into opponent (name, overallResult) values (' .$values. ")";
+  print($sql);
+  $res = mysqli_query($conn, $sql);
+  #$mysqli_free_result($res);
+} else {
+  #そうではない場合、結果を書き換える
+  $new_overall_result = add_quoations(update_overall_result($overall_result, $result));
+  $sql = "update opponent set overallResult=" .$new_overall_result. "where name=" .$opponent;
+  print($sql);
 
 ?>
 
