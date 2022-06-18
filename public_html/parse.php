94d85f4378f6ee40cdb07f482a61947a7c3bd443
Mimori256 <mimorimori256@gmail.com>
2022-06-18 10:24:22 +0900

    add parse.php

---
+++ public_html/parse.php
@@ -0,0 +1,52 @@
+<?php
+
+function form_element($element) {
+  $element = str_replace('[', '', $element);
+  $element = str_replace(']', '', $element);
+  $element = explode(" ", $element, 2)[1];
+  $element = str_replace('"', '', $element);
+  return $element;
+}
+
+
+function parse_moves($moves) {
+  $moves = preg_replace('/[0-9]+\.\s/', '', $moves);
+  $moves = preg_replace('/\s[0-9]+\-[0-9]+/', '', $moves);
+  return $moves;
+}
+
+
+function parse_time($time_range) {
+  $tmp_list = explode("+", $time_range, 2);
+  var_dump($tmp_list);
+  $base_time = intval($tmp_list[0]);
+  $increment = intval($tmp_list[1]);
+
+  #convert base_time to minutes
+  $minute = intval($base_time / 60);
+
+  return strval($minute) . "+" . strval($increment);
+}
+
+
+$filename = 'sample.pgn';
+$contents = file_get_contents($filename);
+
+
+$splited_data = explode("\n", $contents);
+
+$date = form_element($splited_data[2]);
+#$white_player = form_element($splited_data[2]);
+$result = form_element($splited_data[5]);
+$time_range = parse_time(form_element($splited_data[13]));
+$opening = form_element($splited_data[15]);
+$move = parse_moves($splited_data[18]);
+print($move);
+print("\n");
+print($result);
+print("\n");
+print($time_range);
+print("\n");
+print($opening);
+print("\n");
+?>
